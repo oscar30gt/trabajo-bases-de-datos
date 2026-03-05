96a684d729b74aabae188cffab7f5ba286712025
Óscar Grimal Torres <oscar30gt@gmail.com>
2026-03-05 14:06:56 +0100

    consultas

--- BD1/QUERY1.sql
+++ BD1/QUERY1.sql
@@ -2,4 +2,31 @@
 -- se quiere averiguar en qué jornada de cada temporada se marcaron más goles. Listar las temporadas
 -- junto al número de jornada obtenido y el número de goles marcados en dicha jornada.
 
-SELECT ...
+-- Seleccionamos las últimas cinco temporadas de segunda división
+WITH UltimasTemporadas AS (
+    SELECT DISTINCT p1.temporada
+    FROM PARTIDO p1
+    WHERE p1.division = '2' -- Solo segunda división
+    AND ( 
+        SELECT COUNT(*) -- Para cada temporada, contamos cuántas temporadas posteriores hay
+        FROM ( 
+                SELECT DISTINCT p2.temporada
+                FROM PARTIDO p2
+                WHERE p2.division = '2'
+                AND p2.temporada > p1.temporada
+        )
+    ) < 5 -- Solo temporadas que tienen menos de 5 temporadas posteriores (las 5 últimas)
+)
+SELECT p1.temporada, p1.jornada, SUM(p1.golesLocal + p1.golesVisitante) TotalGoles
+FROM PARTIDO p1, UltimasTemporadas u
+WHERE p1.temporada = u.temporada -- Solo partidos de las últimas cinco temporadas
+AND p1.division = '2' -- Solo segunda división
+GROUP BY p1.temporada, p1.jornada -- Agrupamos por temporada y jornada para sumar los goles
+HAVING SUM(p1.golesLocal + p1.golesVisitante) = ( 
+    SELECT MAX(SUM(p2.golesLocal + p2.golesVisitante)) -- Partido con el mayor número de goles en cada temporada
+    FROM PARTIDO p2
+    WHERE p2.temporada = p1.temporada -- Misma temporada
+    AND p2.division = '2' -- Segunda división
+    GROUP BY p2.temporada, p2.jornada
+) -- Seleccionamos solo la/s jornada/s de cada temporada con el máximo número de goles
+ORDER BY p1.temporada DESC;
--- BD1/QUERY2.sql
+++ BD1/QUERY2.sql
@@ -1,4 +1,38 @@
 -- Equipo(s) que han estado en primera división un mínimo de veinticinco temporadas y que no han
 -- ganado ninguna liga.
 
-SELECT ...
+-- Alias con los puntos de cada equipo en cada temporada de primera división
+WITH PuntosTemporada AS (
+    SELECT p.temporada, e.nombreOficial equipo, ((
+			-- Victorias ( +3 puntos )
+			SELECT COUNT(*) FROM PARTIDO pV
+             WHERE pV.temporada = p.temporada AND pV.division = '1'
+               AND ((pV.equipoLocal = e.nombreOficial AND pV.golesLocal > pV.golesVisitante) OR   -- Como local
+                    (pV.equipoVisitante = e.nombreOficial AND pV.golesVisitante > pV.golesLocal)) -- Como visitante
+		) * 3 + (
+			-- Empates ( +1 punto )
+			SELECT COUNT(*) FROM PARTIDO pE
+             WHERE pE.temporada = p.temporada AND pE.division = '1'
+               AND (pE.equipoLocal = e.nombreOficial OR pE.equipoVisitante = e.nombreOficial)
+               AND (pE.golesLocal = pE.golesVisitante) -- Da igual si es local o visitante, el empate es empate
+		)) puntos
+    FROM EQUIPO e, PARTIDO p
+    WHERE (p.equipoLocal = e.nombreOficial OR p.equipoVisitante = e.nombreOficial)
+      AND p.division = '1'
+    GROUP BY p.temporada, e.nombreOficial
+)
+SELECT p1.equipo
+FROM PuntosTemporada p1
+GROUP BY p1.equipo
+HAVING COUNT(*) >= 25 -- Al menos 25 apariciones en primera
+AND p1.equipo NOT IN (
+	-- Subconsulta: Equipos que han sido campeones (máximos puntos de su año)
+	SELECT DISTINCT p2.equipo
+	FROM PuntosTemporada p2
+	WHERE p2.puntos = (
+		SELECT MAX(puntos) 
+		FROM PuntosTemporada p3 
+		WHERE p3.temporada = p2.temporada
+	)
+)
+ORDER BY p1.equipo ASC;
